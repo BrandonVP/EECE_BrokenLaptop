a92c6c447e9b229d11523872789e7c81cb60dc31
Brandon Van Pelt <brandonvanpelt@outlook.com>
2020-09-06 22:24:02 -0500

    Add project files.

---
+++ EECE_BrokenLaptop/app.py
@@ -0,0 +1,148 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+
+# install using,  pip3 install sqlalchemy flask-sqlalchemy 
+from flask_sqlalchemy import SQLAlchemy
+
+# this is the database connection string or link 
+# brokenlaptops.db is the name of database and it will be created inside 
+# project directory. You can choose any other direcoty to keep it, 
+# in that case the string will look different. 
+database = "sqlite:///brokenlaptops.db"
+
+app = Flask(__name__)
+
+# important configuration parameter, don't miss it 
+app.config["SQLALCHEMY_DATABASE_URI"] = database
+
+# database instance. thid db will be used in this project 
+db = SQLAlchemy(app)
+
+##################################################
+# use python shell to create the database (from inside the project directory) 
+# >>> from app import db
+# >>> db.create_all()
+# >>> exit()
+# if you do not do this step, the database file will not be created and you will receive an error message saying "table does not exist".
+###################################################
+
+
+    # Routes users to the home page
+@app.route('/')
+def index():
+    brokenlaptops = BrokenLaptop.query.all()
+    return render_template(
+        "index.html",
+        brokenlaptops=brokenlaptops,
+        title='Home',
+        year=datetime.now().year
+    )
+    
+
+    # Displays laptops currently in the database
+@app.route('/view')
+def view():
+    brokenlaptops = BrokenLaptop.query.all()
+    return render_template(
+        "view.html",
+        brokenlaptops=brokenlaptops,
+        title='View Laptops',
+        year=datetime.now().year
+    )
+
+
+    # Creates entries in the database
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.form:
+        brand = request.form.get("brand")
+        price = request.form.get("price")
+        brokenlaptop = BrokenLaptop(brand=brand,price=price)
+        db.session.add(brokenlaptop)
+        db.session.commit()
+        brokenlaptops = BrokenLaptop.query.all()
+        return render_template("view.html",
+                               brokenlaptops=brokenlaptops,
+                               year=datetime.now().year
+                               )
+    
+    return render_template(
+        "create.html",
+        title='Add Broken Laptop',
+        year=datetime.now().year
+    )
+
+    
+    # Deletes entries in the database
+@app.route('/delete/<laptop_id>')
+def delete(laptop_id):
+    brokenlaptop = BrokenLaptop.query.get(laptop_id)
+    db.session.delete(brokenlaptop)
+    db.session.commit()
+
+    brokenlaptops = BrokenLaptop.query.all()
+    return render_template("view.html",
+                           brokenlaptops=brokenlaptops,
+                           year=datetime.now().year
+                           )
+
+
+    # Updates entries in the database
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+ 
+    if request.method == 'POST':
+        my_data = BrokenLaptop.query.get(request.form.get('id'))
+ 
+        my_data.brand = request.form['brand']
+        my_data.price = request.form['price']
+ 
+        db.session.commit()
+ 
+        brokenlaptops = BrokenLaptop.query.all()
+        return render_template("view.html",
+                               brokenlaptops=brokenlaptops
+                               )
+
+
+    #Renders the contact page
+@app.route('/contact')
+def contact():
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Brandon Van Pelt'
+    )
+
+
+    #Renders the about page
+@app.route('/about')
+def about():
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='This application was developed for a class assignment in EECE 4081'
+    )
+    
+
+# this class creates a table in the database named broken_laptop with 
+# entity fields id as integer, brand as text, and price as decimal number 
+# create a module containing this class and import that class into this application and use it
+class BrokenLaptop(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    brand = db.Column(db.String(40), nullable = False)
+    price = db.Column(db.Float, nullable = True)
+
+
+# Added this code for the flask to work properly in visual studios
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+
